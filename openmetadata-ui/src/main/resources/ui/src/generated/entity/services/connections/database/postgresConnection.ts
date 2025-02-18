308cdb752d2d9e189b8410ce04a14f1124ddc8b8
harshsoni2024 <harshsoni2024@gmail.com>
2025-02-18 15:10:54 +0530

    postgres defaults

--- openmetadata-ui/src/main/resources/ui/src/generated/entity/services/connections/database/postgresConnection.ts
+++ openmetadata-ui/src/main/resources/ui/src/generated/entity/services/connections/database/postgresConnection.ts
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2024 Collate.
+ *  Copyright 2025 Collate.
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
@@ -30,6 +30,14 @@ export interface PostgresConnection {
      * attempts to scan all the databases.
      */
     database: string;
+    /**
+     * List of default databases to exclude in ingestion.
+     */
+    defaultExcludeDatabases?: string[];
+    /**
+     * List of default schemas to exclude in ingestion.
+     */
+    defaultExcludeSchemas?: string[];
     /**
      * Host and port of the source service.
      */
